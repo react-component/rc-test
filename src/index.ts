066c70f511e92f51961b6fe1f57c666ca8ccc59a
二货机器人 <smith3816@gmail.com>
2022-10-20 22:20:46 +0800

    chore: script

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,42 @@
 import jest from 'jest';
 
-export default function() {
-  return new Promise((resolve, reject) => {
-    jest
-      .runCLI(
-        {
-          config: JSON.stringify(config),
-          ...opts,
-        },
-        [cwd],
-      )
-      .then(result => {
-        debug(result);
-        const { results } = result;
-        if (results.success) {
-          resolve();
-        } else {
-          reject(new Error('Jest failed'));
-        }
-      })
-      .catch(e => {
-        console.log(e);
-      });
-  });
+export default function (originOpts: any = {}) {
+  const opts = { ...originOpts };
+  const cwd = process.cwd();
+
+  const config = {
+    rootDir: process.cwd(),
+    setupFiles: [require.resolve('./shim.js')],
+    setupFilesAfterEnv: [require.resolve('./shim.js')],
+    transform: {
+      '\\.(t|j)sx?$': require.resolve('./transformers/jsTransformer'),
+      '\\.svg$': require.resolve('./transformers/fileTransformer'),
+    },
+    // transformIgnorePatterns: [
+    //   // 加 [^/]*? 是为了兼容 tnpm 的目录结构
+    //   // 比如：_umi-test@1.5.5@umi-test
+    //   `node_modules/(?!([^/]*?umi|[^/]*?umi-test|[^/]*?enzyme-adapter-react-16|${transformInclude.join(
+    //     '|',
+    //   )})/)`,
+    // ],
+    testMatch: ['**/?*.(spec|test|e2e).(j|t)s?(x)'],
+    moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json'],
+    // moduleNameMapper: {
+    //   '\\.(css|less|sass|scss)$': require.resolve('identity-obj-proxy'),
+    //   '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+    //     require.resolve('./fileMock.js'),
+    //   ...(moduleNameMapper || {}),
+    //   ...(userModuleNameMapper || {}),
+    // },
+    testPathIgnorePatterns: ['/node_modules/'],
+    // 用于设置 jest worker 启动的个数
+  };
+
+  return jest.runCLI(
+    {
+      config: JSON.stringify(config),
+      ...opts,
+    },
+    [cwd],
+  );
 }
---
+++ src/transformers/fileTransformer.ts
@@ -0,0 +1,25 @@
+import path from "path";
+
+export default {
+  process(src: string, filename: string) {
+    const assetFilename = JSON.stringify(path.basename(filename));
+
+    if (filename.match(/\.svg$/)) {
+      return `module.exports = {
+        __esModule: true,
+        default: ${assetFilename},
+        ReactComponent: ({ svgRef, ...props }) => ({
+          $$typeof: Symbol.for('react.element'),
+          type: 'svg',
+          ref: svgRef || null,
+          key: null,
+          props: Object.assign({}, props, {
+            children: ${assetFilename}
+          })
+        }),
+      };`;
+    }
+
+    return `module.exports = ${assetFilename};`;
+  },
+};
