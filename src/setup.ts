3848900fcc39ca0860d231fa5b1c37fe28ac87a1
afc163 <afc163@gmail.com>
2024-10-15 10:38:40 +0800

    fix jsdom environment
    ref https://github.com/jsdom/jsdom/issues/2524

--- src/setup.ts
+++ src/setup.ts
@@ -1,5 +1,18 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+import * as util from 'util';
+
+// ref: https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
+// ref: https://github.com/jsdom/jsdom/issues/2524
+Object.defineProperty(global, 'TextEncoder', {
+  writable: true,
+  value: util.TextEncoder,
+});
+
+Object.defineProperty(global, 'TextDecoder', {
+  writable: true,
+  value: util.TextDecoder,
+});
 
 /* eslint-disable import/first */
 global.requestAnimationFrame =
